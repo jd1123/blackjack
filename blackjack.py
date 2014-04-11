141a6092eff2b9c3a3281e6fc4234d2cbf032d05
jw <jw@jw-1015E.(none)>
2014-04-11 12:23:06 -0400

    Updated Insurance and improved code readability

--- blackjack.py
+++ blackjack.py
@@ -8,7 +8,9 @@ class Game(object):
 				'deck_count': 6,
 				'default_wager': 5,
 				'starting_bankroll' : 500,
-				'num_decks': 6
+				'num_decks': 6,
+				'blackjack_payout': 1.5,
+				'insurance_payout': 2
 				}
 	
 	def __init__(self):
@@ -23,11 +25,12 @@ class Game(object):
 	
 	#show the count of the deck		
 	def print_count(self):
-		print 'The count is ' + str(self.game_deck.deck_count()) + '....'
+		print 'The count is ' + str(self.game_deck.deck_count()) + '....\n'
 	
 	#check to see if all hands are busts
 	def all_hands_busted(self):
 		b = 1
+		
 		for h in self.play_hand_container:
 			b*=h.busted*1
 		return b
@@ -59,8 +62,10 @@ class Game(object):
 			j+=1
 			
 		dh = ''
+		
 		if reveal:
 			dh = self.dealer_hand.reveal_hand()
+		
 		else:
 			dh = self.dealer_hand.show_hand()
 		
@@ -77,8 +82,13 @@ class Game(object):
 		print "=======================\n"
 		print 'Some info:\n'
 		print 'Cards are shown as (K of D)'
-		print 'The first letter or number is the card, the second is the suit\n'
+		print 'The first letter or number is the card value, the second is the suit\n'
 		print 'D = Diamonds, S = Spades, C = Clubs, H = Hearts\n'
+		print
+		print 'Additional Rules:'
+		print '* Insurance pays ' +str(Game.config['insurance_payout']) + ':1, and is for half the size of your bet'
+		print '* Dealer stands on soft 17s'
+		print '* There are no surrenders'
 		print "=======================\n"
 		self.game_pause()
 	
@@ -97,6 +107,7 @@ class Game(object):
 		os.system('clear')
 		bet_ok = False
 		self.bank_roll.print_bank_roll()
+		
 		while not(bet_ok):	
 			print 'What is your bet? (Default is 5): '
 			bet_input = raw_input()
@@ -116,6 +127,7 @@ class Game(object):
 					bet_ok = True
 				
 			except ValueError:
+				
 				if bet_input.upper() == 'Q':
 					print 'Quitting game...'
 					self.game_state = 'GameOver'
@@ -146,59 +158,109 @@ class Game(object):
 		
 		self.play_hand_container = []
 		
-		self.player_hand.add_card_to_hand(self.game_deck.draw_card())
-		self.dealer_hand.add_card_to_hand(self.game_deck.draw_card())
-		self.player_hand.add_card_to_hand(self.game_deck.draw_card())
-		self.dealer_hand.add_card_to_hand(self.game_deck.draw_card())
+		#self.player_hand.add_card_to_hand(self.game_deck.draw_card())
+		#self.dealer_hand.add_card_to_hand(self.game_deck.draw_card())
+		#self.player_hand.add_card_to_hand(self.game_deck.draw_card())
+		#self.dealer_hand.add_card_to_hand(self.game_deck.draw_card())
 		
-		''' THIS IS FOR DEBUG'' 
+		''' THIS IS FOR DEBUG TO CHECK SPLITS'' 
 		c = self.game_deck.draw_card()
 		self.player_hand.add_card_to_hand(c)
 		self.player_hand.add_card_to_hand(c)
 		##############################'''
 		
+		'''THIS IS FOR DEBUG TO TEST INSURANCE'''
+		self.player_hand.add_card_to_hand([('S',9)])
+		self.player_hand.add_card_to_hand([('S',13)])
+		self.dealer_hand.add_card_to_hand([('D',6)])
+		self.dealer_hand.add_card_to_hand([('D', 14)])
+		#################'''
+		
 		self.play_hand_container.append(self.player_hand)
 		self.show_state()
 		
+		insurance_amount = 0
+		
 		if self.dealer_hand.last_card()[1]==Game.config['Ace']:
-			print 'Would you like insurance? (Y/n)'
-			
+			print 'Would you like insurance? (Y/n)'	
 			flag = True
 			
 			while flag:
 				player_input = raw_input().upper()
 				
 				if player_input == 'Y':
-					insurance = True
-					print 'You have taken insurance.'
-					flag = False
+					insurance_amount = int(self.player_hand.wager/2)
+					if self.bank_roll.has_enough(self.player_hand.wager + insurance_amount):
+						insurance = True
+						print 'You have taken insurance for ' + str(insurance_amount)
+						flag = False
+						self.game_pause()
+						os.system('clear')
+					
+					else:
+						print 'You do not have enough for insurance\n'
+						flag = False
+						insurance = False
+						
 				elif player_input == 'N':
-					print 'You have declined insurance'
+					print 'You have declined insurance\n'
+					self.game_pause()
+					os.system('clear')
 					flag = False
+					self.show_state(msg = 'You have bet ' + str(self.player_hand.wager))
+				
 				else:
 					print 'You entered something incorrectly. Please type (Y) for yes or (N) for no.'
 		
 		if self.dealer_hand.reveal_value()==21:
-			self.show_state('Dealer has 21', reveal=True)
-			print 'Dealer has 21, you lose :('
-			self.bank_roll.dec(self.player_hand.wager)
-			if insurance:
-				self.bank_roll.inc(2*self.player_hand.wager)
-				print 'But you had insurance!'
-			self.game_pause()
-			return False
-		else:
-			if self.player_hand.hand_value()==21:
-				print 'BLACKJACK!'
-				print 'You win ' + str(int(self.player_hand.wager*1.5))
-				self.bank_roll.inc(int(self.player_hand.wager*1.5))
+			
+			if (self.player_hand.hand_value() == 21):
+				print 'Push'
 				if insurance:
-					self.bank_roll.dec(self.player_hand.wager)
+					insurance_payout = Game.config['insurance_payout']*insurance_amount
+					self.bank_roll.inc(insurance_payout)
+					print 'But you had insurance! You win ' + str(insurance_payout) + '.\n'
+				
+				else:
+					print '**** No Money Exchanged ****'
+				
 				self.game_pause()
 				return False
+			
+			else:
+				self.show_state('Dealer has 21', reveal=True)
+				print 'Dealer has 21, you lose ' + str(self.player_hand.wager)
+				self.bank_roll.dec(self.player_hand.wager)
+				if insurance:
+					insurance_payout = Game.config['insurance_payout']*insurance_amount
+					self.bank_roll.inc(insurance_payout)
+					print 'But you had insurance! You win ' + str(insurance_payout)
+				
+				self.game_pause()
+				return False
+		else:
+			
+			if self.player_hand.hand_value()==21:
+				print 'BLACKJACK!'
+				print 'You win ' + str(self.player_hand.wager*Game.config['blackjack_payout'])
+				self.bank_roll.inc(self.player_hand.wager*Game.config['blackjack_payout'])
+				if insurance:
+					self.bank_roll.dec(insurance_amount)
+					print 'Your bankroll has been reduced by ' + str(insurance_amount) + ' for insurance'
+				
+				self.game_pause()
+				return False
+			
 			else:
 				if insurance:
-					self.bank_roll.dec(self.player_hand.wager)
+					self.bank_roll.dec(insurance_amount)
+					print 'The dealer does not have blackjack.'
+					print 'Your bankroll has been reduced by the insurance wager of ' + str(insurance_amount)
+					print 'You now have: ' + str(self.bank_roll.balance)
+					self.game_pause()
+					os.system('clear')
+					self.show_state(msg = 'You have bet ' + str(self.player_hand.wager) )
+				
 				return True
 	
 	#This is the dealer logic for after the player makes all decisions
@@ -206,19 +268,23 @@ class Game(object):
 	def dealer_logic(self):
 		dealer_go = True
 		self.game_pause()
+		
 		while(dealer_go):
+			
 			if self.dealer_hand.reveal_value() < 17:
 				print 'Dealer Hits!'
 				print 'Dealer Draws: ' + self.dealer_hand.hit(self.game_deck)
 				print 'Dealer Hand: ' + self.dealer_hand.reveal_hand() + '\n'
+			
 			elif self.dealer_hand.reveal_value() > 21:
 				print 'Dealer Busts!'
 				print 'Dealer hand: ' + str(self.dealer_hand.reveal_hand()) + '\n'
 				dealer_go=False
 				return self.dealer_hand.reveal_hand()
+			
 			else:
 				print 'Dealer Stands!'
-				print 'Dealer hand: ' + str(self.dealer_hand.reveal_hand()) + '\n'
+				#print 'Dealer hand: ' + str(self.dealer_hand.reveal_hand()) + '\n'
 				dealer_go=False
 				return self.dealer_hand.reveal_hand()
 			
@@ -232,9 +298,9 @@ class Game(object):
 		dlr = self.dealer_hand.reveal_value()
 			
 		if plr > 21:
-			#self.show_state('Bust', reveal = True)
 			print 'Player Busts!\n'
 			plrhand.busted = True
+			
 			if dealer_done:	
 				print '**** Player loses ' + str(plrhand.wager) + '.\n'
 				self.bank_roll.dec(plrhand.wager)
@@ -242,20 +308,24 @@ class Game(object):
 			return False
 			
 		if dealer_done:	
+			
 			if(dlr>21):
 				print 'Dealer Busts!'
 				print '**** Player wins ' + str(plrhand.wager) + '. ****\n'
 				self.bank_roll.inc(plrhand.wager)
 				return False
+			
 			elif (dlr==plr):
 				print 'Push\n'
 				print '**** No Money Exchanged. ****'
 				return False
+			
 			elif (dlr>plr):
 				print 'Dealer Wins :('
 				print '**** Player loses ' + str(plrhand.wager) + '. ****\n'
 				self.bank_roll.dec(plrhand.wager)
 				return False
+			
 			elif (plr>dlr):
 				print '**** Player wins: ' + str(plrhand.wager) + '. ****\n'
 				self.bank_roll.inc(plrhand.wager)
@@ -266,8 +336,11 @@ class Game(object):
 	#Get player input for each hand
 	def player_logic(self, player_go):
 		hand_index=0
+		
 		for h in self.play_hand_container:
+			
 			while h.hand_active:		
+				
 				if len(self.play_hand_container)>1:
 					print 'Active Hand: ' + str(hand_index)
 				
@@ -300,7 +373,7 @@ class Game(object):
 					
 					else:
 						print 'You do not have that much cash, and so you cannot double down'
-						print 'Please try again.'
+						print 'Please try again.\n'
 				
 				elif player_input == 'Q':
 					print 'Quitting game...'
@@ -375,7 +448,7 @@ class Game(object):
 					self.evaluate_hand(h, dealer_done = True, msg=msg)
 					self.game_pause()
 		
-	#Small, rudimentary game "engine"
+	#The main loop
 	def run_game(self):
 		while (self.run_flag):
 			
