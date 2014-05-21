ad4b56151c3bf2ebb191d0f0f65cf7c50b1557a7
John Wasack <johnnydiabetic@gmail.com>
2014-05-21 13:11:45 -0400

    More bugs with returning a list of a card instead of a card itself:
    * This time in the pop_card method of the Hand object. Thank you testing!

--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-from bjcore import MultiDeck
+from bjcore import MultiDeck, Hand
 import unittest
 import sys
 
@@ -15,7 +15,20 @@ class MultiDeckTest(unittest.TestCase):
         self.assertEqual(len(multi_deck.deck), 4*52, 'The amount of cards does not equal decks * 52' )
         self.assertTrue((c in multi_deck.deck), 'The drawn card is not in the reshuffled deck')
         self.assertEqual(d_pile, [], 'The discard pile is not empty on reshuffle')    
+
+class HandTest(unittest.TestCase):
     
+    def setUp(self):
+        self.my_deck = MultiDeck(4)
+        self.hand = Hand()
+    
+    def test_addingcards(self):
+        card = self.my_deck.draw_card()
+        self.hand.add_card_to_hand(card)
+        self.assertEqual(len(self.hand.cards_in_hand),1, 'There is not one and only one card in the hand after adding one')
+        self.assertTrue(card==self.hand.pop_card(), 'pop_card did not return the same object as the last card in hand')
+        self.assertEqual(len(self.hand.cards_in_hand), 0, 'pop_card did not remove the card in hand')
+        
 def main():
     unittest.main()
     
